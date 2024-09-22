98c3966e46c4bc58bd6b2e09839b015bb8666cfc
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-09-22 11:35:55 +0530

    changes

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef }from 'react';
 import './contact.css';
 import Walmart from '../../assets/walmart.png';
 import Adobe from '../../assets/adobe.png';
@@ -6,9 +6,24 @@ import Microsoft from '../../assets/microsoft.png';
 import Facebook from '../../assets/facebook.png';
 import FacebookIcon from '../../assets/facebook-icon.png';
 import TwitterIcon from '../../assets/twitter.png';
-import YoutubeIcon from '../../assets/youtube.png';
+import LinkedinIcon from '../../assets/linkedin.png';
 import InstagramIcon from '../../assets/instagram.png';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+     e.preventDefault();
+
+     emailjs.sendForm('service_7ka1gsh', 'template_28zxw4a', form.current, 's2WPTF3o_ZZ6PwVXgEaUW')
+         .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+          alert('Email Sent! ');
+        }, (error) => {
+          console.log(error.text);
+      });
+    };
   return (
    <section id='contactPage'>
     <div id='clients'>
@@ -27,16 +42,16 @@ const Contact = () => {
     <div id='contact'>
         <h1 className='contactPageTitle'>Contact Me</h1>
         <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
-        <form className='contactForm'>
-            <input type='text' className='name' placeholder='Your Name' />
-            <input type='email' className='email' placeholder='Email Address'/>
+        <form className='contactForm' ref={form} onSubmit={sendEmail}>
+            <input type='text' className='name' placeholder='Your Name' name='your_name' />
+            <input type='email' className='email' placeholder='Email Address' name='your_email'/>
             <textarea className='msg' name='message' rows='5' placeholder='Your Message'></textarea>
             <button type="submit" value='Send' className='submitBtn'>Submit</button>
             <div className='links'>
-                <img src={FacebookIcon} alt='Facebook' className='link' />
-                <img src={TwitterIcon} alt='Twitter' className='link' />
-                <img src={YoutubeIcon} alt='Youtube' className='link' />
-                <img src={InstagramIcon} alt='Instagram' className='link' />
+                <img src={FacebookIcon} alt='Facebook' className='https://www.facebook.com/profile.php?id=100045383851127' />
+                <img src={TwitterIcon} alt='Twitter' className='https://twitter.com/shash90591?t=CgbYPdOoeTr46Nn2c7dUiw&s=08' />
+                <img src={LinkedinIcon} alt='Linkedin' className='https://www.linkedin.com/in/shashi-shekhar-dixit-266aa825a/' />
+                <img src={InstagramIcon} alt='Instagram' className='https://www.instagram.com/saurabh_dixit_80_/' />
             </div>
         </form>
     </div>
